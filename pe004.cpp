c7dc14b05631dd616688bcb1033c3ef455f69857
BestUsername <marcster@gmail.com>
2015-01-30 12:50:55 -0500

    Added problem 4

---
+++ pe004.cpp
@@ -0,0 +1,62 @@
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <memory>
+#include <cmath>
+/*
+A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+
+Find the largest palindrome made from the product of two 3-digit numbers.
+*/
+
+bool is_pal_brute(int num) {
+    bool retval = true;
+    std::string text = std::to_string(num);
+    //if odd length (eg 12021), no need to compare centre 0 to anything
+    int num_digits_check = text.length() / 2;
+    for (int index = 0; index < num_digits_check && retval; ++index) {
+        if (text[index] != text[text.length()-1-index]) {
+            retval = false;
+        }
+    }
+    return retval;
+}
+
+
+std::shared_ptr<std::pair<int, int> >
+largest_palindrome_brute(int num_digits) {
+    std::shared_ptr<std::pair<int, int> > retval(new std::pair<int,int>());
+    if (num_digits > 0) {
+        int start = std::pow(10, num_digits - 1);
+        int end = std::pow(10, num_digits) - 1;
+        int max_product = 0;
+        int p1 = 0;
+        int p2 = 0;
+        int temp = 0;
+        for (int i = start; i <= end; ++i) {
+            for (int o = start; o <= end; ++o) {
+                temp = i * o;
+                if (temp > max_product) {
+                    if (is_pal_brute(temp)) {
+                        p1 = i;
+                        p2 = o;
+                        max_product = temp;
+                    }
+                }
+            }
+        }
+        retval->first = p1;
+        retval->second = p2;
+    }
+    return retval;
+}
+
+int main(int argc, char **argv) {
+    int retval = EXIT_SUCCESS;
+    int ans = 906609;
+    std::shared_ptr<std::pair<int, int> > solve = largest_palindrome_brute(3);
+    int value = solve->first * solve->second;
+    std::cout << "Answer should be " << ans << ": " << value << std::endl;
+    return retval;
+}
+

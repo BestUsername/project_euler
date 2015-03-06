c29656ee2a08c570c3e294de31863b28aef41bf3
BestUsername <marcster@gmail.com>
2015-03-06 14:21:58 -0500

    Added problem 16

---
+++ pe016.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include <cstdlib>
+#include <vector>
+#include <cmath>
+#include <sstream>
+
+template<typename T>
+std::string get_reverse_buffer_string(const std::vector<T>& buffer) {
+    std::stringstream result;
+    for (int i = buffer.size()-1; i>=0; --i) {
+        result << buffer[i];
+    }
+    return result.str();
+}
+
+size_t sum_power_digits(size_t exponent) {
+    size_t result = 0;
+    //No native data type large enough for 2^1000
+    //Must use container. Might as well use vector
+    std::vector<size_t> buffer(1);
+    buffer[0] = 1;
+    //max_index stores the position of our most significant digit.
+    size_t max_index = 0;
+    size_t digit = 0;
+    size_t x2 = 0;
+    size_t sum_next = 0;
+    for (size_t e = 1; e <= exponent; ++e) {
+        for (size_t i = 0; i <= max_index; ++i) {
+            digit = buffer[i];
+            x2 = digit * 2;
+            
+            sum_next /=10;
+            buffer[i] = x2 % 10 + sum_next %10;
+            sum_next += x2;
+
+            if (max_index < i + (sum_next ==0 ? 0 : (size_t)(std::log10(sum_next)))) {
+                size_t s = (size_t)(i + std::log10(sum_next));
+                max_index = std::max(max_index, s);
+                buffer.resize(s+1);
+            }
+        }
+        //std::cout << "2^" << e << ": " << get_reverse_buffer_string(buffer) << std::endl;;
+    }
+    for (size_t i = 0; i <= max_index; ++i) {
+        result += buffer[i];
+    }
+    return result;
+}
+
+int main(int argc, char ** argv) {
+    int exit_code = EXIT_SUCCESS;
+    int ans = 1366;
+    int result = sum_power_digits(1000);
+    std::cout << "Answer should be " << ans << ":\t" << result << std::endl;
+    return exit_code;
+}
+
